4dff0f3e1eaefa93356b632d62fc4252f4b69692
maryam-alizadeh <maryamaliizadeh1999@gmail.com>
2025-10-02 13:36:16 +0330

    finally

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const searchInput = document.getElementById("search-input");
 const products = document.querySelectorAll(".product-item");
 const buttons = document.querySelectorAll(".filter");
+const priceButton = document.getElementById("search-price").querySelector("button");
+console.log(priceButton);
 
 const changeClass = (filter) => {
   buttons.forEach((button) => {
@@ -31,8 +33,29 @@ const filterHandler = (event) => {
   });
 };
 
-searchInput.addEventListener("keyup", searchHandler);
+const searchPriceHandler = (event) => {
+  const searchPrice = +event.target.parentElement.children[0].value;
 
-buttons.forEach((button) => {
-  button.addEventListener("click", filterHandler);
-});
+  products.forEach((product) => {
+    const productPrice = product.children[2].innerText;
+    const price = +productPrice.split(" ")[1];
+
+    if (!searchPrice) {
+      product.style.display = "block";
+    } else {
+      searchPrice === price ? (product.style.display = "block") : (product.style.display = "none");
+    }
+  });
+};
+
+const start = () => {
+  buttons.forEach((button) => {
+    button.addEventListener("click", filterHandler);
+  });
+
+  searchInput.addEventListener("keyup", searchHandler);
+
+  priceButton.addEventListener("click", searchPriceHandler);
+};
+
+window.addEventListener("load", start);
