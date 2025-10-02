910cc9c98ecb52331c6cc4c1060de430aa8547ce
maryam-alizadeh <maryamaliizadeh1999@gmail.com>
2025-10-02 12:05:22 +0330

    add categories and filter button

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
+const searchInput = document.getElementById("search-input");
+const products = document.querySelectorAll(".product-item");
+const buttons = document.querySelectorAll(".filter");
 
+const searchHandler = (event) => {
+  const searchValue = event.target.value.toLowerCase().trim();
+
+  products.forEach((product) => {
+    const productName = product.children[1].innerText.toLowerCase();
+
+    productName.includes(searchValue) ? (product.style.display = "block") : (product.style.display = "none");
+  });
+};
+const filterHandler = (event) => {
+  const filter = event.target.dataset.filter;
+
+  products.forEach((product) => {
+    const category = product.dataset.category;
+    filter === "all" ? (product.style.display = "block") : filter === category ? (product.style.display = "block") : (product.style.display = "none");
+  });
+};
+
+searchInput.addEventListener("keyup", searchHandler);
+buttons.forEach((button) => {
+  button.addEventListener("click", filterHandler);
+});
